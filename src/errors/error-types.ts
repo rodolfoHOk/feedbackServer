07df48f8018497ee28859baf5ca743b25d5c06d8
rodolfoHOk <rodolfo.okino@gmail.com>
2022-05-16 21:17:04 -0300

    add forbidden to error types

--- src/errors/error-types.ts
+++ src/errors/error-types.ts
@@ -1,6 +1,7 @@
-export const ErroTypes = {
+export const ErrorTypes = {
   AUTHENTICATION_ERROR: 'Authentication error',
   BUSINESS_ERROR: 'Business error',
+  FORBIDDEN_ERROR: 'Forbidden',
   INTERNAL_SERVER_ERROR: 'Internal server error',
   INVALID_DATA: 'Invalid data',
   RESOURCE_NOT_FOUND: 'Resource not found',
